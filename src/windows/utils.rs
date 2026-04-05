7f527f89b5912a99891343df33e1ac605d66c04e
m <autoacceleration42@gmail.com>
2026-04-05 23:27:35 +0300

    ?

--- src/windows/utils.rs
+++ src/windows/utils.rs
@@ -1,20 +1,9 @@
-use windows::core::GUID;
 use windows::Win32::Foundation::{CloseHandle, HANDLE};
 use windows::Win32::Media::Audio::IMMDevice;
-use windows::Win32::System::Com::StructuredStorage::PropVariantClear;
-use windows::Win32::System::Com::StructuredStorage::PROPVARIANT;
-use windows::Win32::System::Com::{
-    CoInitializeEx, CoUninitialize, COINIT_APARTMENTTHREADED, STGM_READ,
-};
+use windows::Win32::System::Com::{CoInitializeEx, CoUninitialize, COINIT_APARTMENTTHREADED};
 
 use windows::Win32::System::ProcessStatus::K32GetModuleBaseNameW;
 use windows::Win32::System::Threading::OpenProcess;
-use windows::Win32::UI::Shell::PropertiesSystem::PROPERTYKEY;
-
-pub const PKEY_DEVICE_FRIENDLY_NAME: PROPERTYKEY = PROPERTYKEY {
-    fmtid: GUID::from_u128(0xa45c254e_df1c_4efd_8020_67d146a85090),
-    pid: 14,
-};
 
 pub unsafe fn get_process_name(pid: u32) -> String {
     let access = windows::Win32::System::Threading::PROCESS_QUERY_INFORMATION
@@ -42,47 +31,17 @@ pub unsafe fn get_process_name(pid: u32) -> String {
     }
 }
 
-unsafe fn prop_variant_to_string(variant: &PROPVARIANT) -> String {
-    match variant.Anonymous.Anonymous.vt.0 {
-        8 => {
-            let bstr = variant.Anonymous.Anonymous.Anonymous.bstrVal;
-            if bstr.is_null() {
-                String::new()
-            } else {
-                bstr.to_string()
-            }
-        }
-        31 => {
-            let psz = variant.Anonymous.Anonymous.Anonymous.pszVal;
-            if psz.is_null() {
-                String::new()
-            } else {
-                psz.to_string().unwrap_or_default()
-            }
-        }
-        _ => String::new(),
-    }
-}
-
 pub unsafe fn get_device_name(device: &IMMDevice) -> String {
-    let prop_store = match device.OpenPropertyStore(STGM_READ) {
-        Ok(store) => store,
-        Err(_) => return "Default Device".to_string(),
-    };
-
-    let mut variant = match prop_store.GetValue(&PKEY_DEVICE_FRIENDLY_NAME) {
-        Ok(v) => v,
-        Err(_) => return "Default Device".to_string(),
-    };
-
-    let name = prop_variant_to_string(&variant);
-
-    let _ = PropVariantClear(&mut variant);
-
-    if name.is_empty() {
-        "Default Device".to_string()
-    } else {
-        name
+    match device.GetId() {
+        Ok(id) => {
+            let name = id.to_string().unwrap_or_default();
+            if name.is_empty() {
+                "Default Device".to_string()
+            } else {
+                name
+            }
+        }
+        Err(_) => "Default Device".to_string(),
     }
 }
 
